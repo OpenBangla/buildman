7563bb02a191c00557f8ea0234a2eac536298c05
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-13 20:47:59 +0600

    This might work

--- script.sh
+++ script.sh
@@ -4,10 +4,13 @@ if [[ $DIST = "ubuntu17.10" ]]; then
     docker pull ubuntu:17.10
     docker run -itd --name build ubuntu:17.10
     echo "ubuntu17.10"
-    docker exec build sh -C makedeb.sh
 elif [[ $DIST = "ubuntu16.04" ]]; then
     docker pull ubuntu:16.04
     docker run -itd --name build ubuntu:16.04
     echo "ubuntu16.04"
-    docker exec build sh -C makedeb.sh
 fi
+
+docker exec build apt-get update
+docker exec build apt-get -y install git
+docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+docker exec build /ci/makedeb.sh
