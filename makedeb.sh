ed17db82405727fa2111474529f35424d589f8a0
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-13 23:25:38 +0600

    Use Qt 5.9.1

--- makedeb.sh
+++ makedeb.sh
@@ -3,7 +3,7 @@ export DIST=$1
 if [[ $DIST = "ubuntu16.04" ]]; then
     # Install Qt 5.9 for Ubuntu 16.04
     apt-get -y install software-properties-common
-    add-apt-repository ppa:beineri/opt-qt59-xenial
+    add-apt-repository ppa:beineri/opt-qt591-xenial
     apt-get update -qq
     apt-get -y install qt59base qt59declarative
     source /opt/qt59/bin/qt59-env.sh
