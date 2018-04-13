67dd12ea3b4b802f05d42725027574cf42f7dfce
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-13 20:03:39 +0600

    use sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ if [[ $DIST = "ubuntu17.10" ]]; then
     docker pull ubuntu:17.10
     docker run -itd --name build ubuntu:17.10
     echo "ubuntu17.10"
-    docker exec build makedeb.sh
+    docker exec build sh -C makedeb.sh
 elif [[ $DIST = "ubuntu16.04" ]]; then
     docker pull ubuntu:16.04
     docker run -itd --name build ubuntu:16.04
     echo "ubuntu16.04"
-    docker exec build makedeb.sh
+    docker exec build sh -C makedeb.sh
 fi
