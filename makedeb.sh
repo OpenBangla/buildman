cc79a81ec0156906281bfb628918dcd284f70825
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-13 21:37:39 +0600

    Alhumdulillah, fix Ubuntu 16.04 build! :tada:

--- makedeb.sh
+++ makedeb.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get -y install git build-essential cmake libibus-1.0-dev qt5-default qtdeclarative5-dev ninja-build
+if [[ $DIST = "ubuntu16.04" ]]; then
+    # Install Qt 5.9 for Ubuntu 16.04
+    add-apt-repository --yes ppa:beineri/opt-qt591-trusty
+    apt-get update -qq
+    apt-get -y install qt59base qt59declarative
+    source /opt/qt59/bin/qt59-env.sh
+elif [[ $DIST = "ubuntu17.10" ]]; then
+    apt-get update -qq
+    apt-get -y install qt5-default qtdeclarative5-dev
+fi
+apt-get -y install build-essential cmake libibus-1.0-dev ninja-build
 git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /repo
 cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=DEB
 ninja package -C build
