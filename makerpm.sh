477262e5750af44cf6550884a5837213397ca608
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-14 00:10:58 +0600

    Support Fedora 27

---
+++ makerpm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export DIST=$1
+dnf install @buildsys-build cmake qt5-qtdeclarative-devel ibus-devel ninja-build
+git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /repo
+cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=RPM
+ninja package -C build
--- script.sh
+++ script.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
+makeDeb() {
+    docker exec build apt-get update
+    docker exec build apt-get -y install git
+    docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+    docker exec build chmod +x /ci/makedeb.sh
+    docker exec build /ci/makedeb.sh $DIST
+}
+
 if [[ $DIST = "ubuntu17.10" ]]; then
     docker pull ubuntu:17.10
     docker run -itd --name build ubuntu:17.10
-    echo "ubuntu17.10"
+    makeDeb
 elif [[ $DIST = "ubuntu16.04" ]]; then
     docker pull ubuntu:16.04
     docker run -itd --name build ubuntu:16.04
-    echo "ubuntu16.04"
+    makeDeb
+elif [[ $DIST = "fedora27" ]]; then
+    docker pull fedora:27
+    docker run -itd --name build fedora:27
+    docker exec build dnf install git
+    docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+    docker exec build chmod +x /ci/makerpm.sh
+    docker exec build /ci/makerpm.sh $DIST
 fi
-
-docker exec build apt-get update
-docker exec build apt-get -y install git
-docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
-docker exec build chmod +x /ci/makedeb.sh
-docker exec build /ci/makedeb.sh $DIST
