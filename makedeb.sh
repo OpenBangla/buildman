ac3e06b1fa91553e63328545fed24eccad8592f4
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-15 23:37:19 +0600

    install curl

--- makedeb.sh
+++ makedeb.sh
@@ -12,7 +12,7 @@ elif [[ $DIST = "ubuntu17.10" ]]; then
     apt-get update -qq
     apt-get -y install qt5-default qtdeclarative5-dev
 fi
-apt-get -y install build-essential cmake libibus-1.0-dev ninja-build
+apt-get -y install build-essential cmake libibus-1.0-dev ninja-build curl
 git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /repo
 cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=DEB
 ninja package -C build
