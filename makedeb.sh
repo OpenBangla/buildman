2f0f7225be8ae5f35a473da2592f26524e0dfba8
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-20 14:27:38 +0600

    deploy rpm stop deploying for ubuntu for now

--- makedeb.sh
+++ makedeb.sh
@@ -5,4 +5,4 @@ apt-get -y install build-essential cmake libibus-1.0-dev ninja-build curl qt5-de
 git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /repo
 cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=DEB
 ninja package -C build
-curl --upload-file build/${RELPACK}${DIST}.deb https://transfer.sh/${RELPACK}${DIST}.deb
+#curl --upload-file build/${RELPACK}${DIST}.deb https://transfer.sh/${RELPACK}${DIST}.deb
--- makerpm.sh
+++ makerpm.sh
@@ -5,8 +5,4 @@ dnf install -y @buildsys-build cmake qt5-qtdeclarative-devel ibus-devel ninja-bu
 git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /repo
 cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=RPM
 ninja package -C build
-echo "${RELPACK}${DIST}.rpm"
-source /ci/transfer.sh
-#curl --upload-file build/${RELPACK}${DIST}.rpm "https://transfer.sh/${RELPACK}${DIST}.rpm"
-transfer build/${RELPACK}${DIST}.rpm
-file build/${RELPACK}${DIST}.rpm
+curl --upload-file build/${RELPACK}${DIST}.rpm https://transfer.sh/${RELPACK}${DIST}.rpm
