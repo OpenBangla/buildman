f56b6a647313671d70cac0db27b9489bba201ed4
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-06-21 14:19:20 +0600

    Debug deploy+

--- makearch.sh
+++ makearch.sh
@@ -11,6 +11,8 @@ echo -e "pkgver=\"$RELVER\"\n$(cat /ci/PKGBUILD.stub)" > PKGBUILD
 chown nobody:nobody /build
 sudo -u nobody makepkg -fd --skipinteg
 mv openbangla-keyboard-*.pkg.tar.xz ${RELPACK}${DIST}.pkg.tar.xz
+echo "ls!"
+ls
 if [ $DEPLOY == 1 ]; then
     echo "Deploying artifacts to transfer.sh"
     curl --upload-file ${RELPACK}${DIST}.pkg.tar.xz https://transfer.sh/${RELPACK}${DIST}.pkg.tar.xz
