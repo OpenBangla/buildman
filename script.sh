a04b6561081d8ba786529e29dd9603271f6b5c90
bdeshi <email@bdeshi.space>
2020-01-29 22:50:02 +0600

    fork-friendly CI

--- script.sh
+++ script.sh
@@ -6,10 +6,13 @@ export RELPACK="OpenBangla-Keyboard_$RELVER-"
 string='deploy+'
 export DEPLOY=$(git log -1 --pretty=%B | grep "$string" -q  && echo 1 || echo 0)
 
+export REPO=${TRAVIS_PULL_REQUEST_SLUG:-$TRAVIS_REPO_SLUG}
+export BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+
 makeDeb() {
     docker exec build apt-get -qq update
     docker exec build apt-get -y install git
-    docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+    docker exec build git clone https://github.com/$REPO.git -b $BRANCH /ci
     docker exec build /ci/makedeb.sh $DIST $RELPACK $DEPLOY
 }
 
@@ -25,13 +28,13 @@ elif [[ $DIST = "fedora28" ]]; then
     docker pull fedora:28
     docker run -itd --name build fedora:28 /bin/bash
     docker exec build dnf -y install git
-    docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+    docker exec build git clone https://github.com/$REPO.git -b $BRANCH /ci
     docker exec build /ci/makerpm.sh $DIST $RELPACK $DEPLOY
 elif [[ $DIST = "archlinux" ]]; then
     docker pull archlinux/base
     docker run -itd --name build archlinux/base
     docker exec build pacman -Syyu --noconfirm --needed
     docker exec build pacman -S --noconfirm --needed base git
-    docker exec build git clone https://github.com/OpenBangla/buildman.git /ci
+    docker exec build git clone https://github.com/$REPO.git -b $BRANCH /ci
     docker exec build /ci/makearch.sh $DIST $RELPACK $RELVER $DEPLOY
 fi
