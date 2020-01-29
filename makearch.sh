e26f456604b2bfecdc60108b35c4c546d88a13c2
bdeshi <email@bdeshi.space>
2020-01-29 23:43:47 +0600

    simplify deploy command

--- makearch.sh
+++ makearch.sh
@@ -14,5 +14,5 @@ sudo -u builder makepkg -fd --skipinteg
 mv openbangla-keyboard-*.pkg.tar.xz ${RELPACK}${DIST}.pkg.tar.xz
 if [ $DEPLOY == 1 ]; then
     echo "Deploying artifacts to transfer.sh"
-    curl --upload-file ${RELPACK}${DIST}.pkg.tar.xz https://transfer.sh/${RELPACK}${DIST}.pkg.tar.xz
+    curl --upload-file ${RELPACK}${DIST}.pkg.tar.xz https://transfer.sh/
 fi
--- makedeb.sh
+++ makedeb.sh
@@ -10,5 +10,5 @@ cmake -H/repo -B/build -GNinja -DCPACK_GENERATOR=DEB
 ninja package -C /build
 if [ $DEPLOY == 1 ]; then
     echo "Deploying artifacts to transfer.sh"
-    curl --upload-file /build/${RELPACK}${DIST}.deb https://transfer.sh/${RELPACK}${DIST}.deb
+    curl --upload-file /build/${RELPACK}${DIST}.deb https://transfer.sh/
 fi
