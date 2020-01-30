4e1423322f764bc1a1e7328e487e6c558611b6f3
bdeshi <email@bdeshi.space>
2020-01-30 08:42:38 +0600

    safe arch build

--- makearch.sh
+++ makearch.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-export DIST=$1
+export DIST=archlinux
 export RELPACK=$2
 export RELVER=$3
 export DEPLOY=$4
+# pin package extension
+export PKGEXT=".pkg.tar.xz"
 pacman -S --noconfirm --needed base-devel cmake libibus qt5-base rust curl
 mkdir /build
-cd /build
-echo -e "pkgver=\"$RELVER\"\n$(cat /ci/PKGBUILD.stub)" > PKGBUILD
-# makepkg does not run as root
+echo -e "pkgver=\"$RELVER\"\n$(cat /ci/PKGBUILD.stub)" > /build/PKGBUILD
 useradd -m builder
-chown builder:builder /build
+chown -R builder:builder /build
+cd /build
 sudo -u builder makepkg -fd --skipinteg
-mv openbangla-keyboard-*.pkg.tar.xz ${RELPACK}${DIST}.pkg.tar.xz
+mv openbangla-keyboard-*${PKGEXT} ${RELPACK}${DIST}${PKGEXT}
 if [ $DEPLOY == 1 ]; then
     echo "Deploying artifacts to transfer.sh"
-    curl --upload-file ${RELPACK}${DIST}.pkg.tar.xz https://transfer.sh/
+    curl --upload-file ${RELPACK}${DIST}${PKGEXT} https://transfer.sh/
 fi
