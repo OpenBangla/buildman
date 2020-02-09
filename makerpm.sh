ddd76055f86e2cda7be1ce1478096e4e2e543590
bdeshi <email@bdeshi.space>
2020-02-09 12:50:09 +0600

    bintray deployment | pristine

--- makerpm.sh
+++ makerpm.sh
@@ -2,9 +2,8 @@
 RELEASE_STUB=$1
 RELEASE_FILENAME="${RELEASE_STUB}${DIST}.rpm"
 dnf install -y --allowerasing @buildsys-build cmake ibus-devel qt5-qtdeclarative-devel rust cargo ninja-build
-git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git -b 1.5.1 /src
+git clone https://github.com/OpenBangla/OpenBangla-Keyboard.git /src
 git -C /src submodule update --init --recursive
-sed -i '/PACKAGE_VENDOR/c set(CPACK_PACKAGE_VENDOR "OpenBangla")' /src/CMakeLists.txt
 cmake -H/src -B/build -GNinja -DCPACK_GENERATOR=RPM
 ninja package -C /build
 if [ $DEPLOY == true ]; then
